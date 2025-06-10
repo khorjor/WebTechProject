409d2ddcb82c3189d7e419e420656635d6d30a4d
khorjor <khaojaoshakeaw@gmail.com>
2025-06-10 19:27:24 +0700

    Login, Register

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+
+app.use('/api/login', require('./routes/login'));
+app.use('/api/register', require('./routes/register'));
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
